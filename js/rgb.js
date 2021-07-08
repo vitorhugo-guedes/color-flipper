dfbd2751a0fc0e1d07609538248643c591514036
vitorhugo-guedes <vitoroliveira.guedes@outlook.com>
2021-07-08 20:26:58 -0300

    Update
    Add the rgb random generator.

--- js/rgb.js
+++ js/rgb.js
@@ -2,18 +2,24 @@ const btn = document.getElementById('btn');
 const showColor = document.querySelector('.color');
 
 btn.addEventListener('click', () =>{
-    let color = [];
-    for(var i = 0; i < 2; i++){
-       color += generateRandom(0, 255);
-    }
+    let color = generateRGB();
     showColor.textContent = color;
     document.body.style.backgroundColor = color;
 })
 
 
-//gera um numero entre intervalos e inclui os intervalos.
+//generate a random number and include the intervals.
 function generateRandom(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//More simple to show to the user
+function generateRGB(){
+    let r = generateRandom(0, 255);
+    let g = generateRandom(0, 255);
+    let b = generateRandom(0, 255);
+
+    return `rgb(${r}, ${g}, ${b})`;
 }
